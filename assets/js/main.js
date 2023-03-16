96db75287777fc6bc48767d0d6d3acc95292f09b
bryansuarez2004 <bryansf2004@gmail.com>
2023-03-16 12:17:10 -0500

    carrito casi listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ async function getProducts(){
     const productsHTML = document.querySelector('.products');
    let html = ''
     for (const product of bDatos.products) {
-        console.log(product);
+        
 
         html += `
          <div class='product'>
@@ -28,8 +28,13 @@ async function getProducts(){
          <div class="product_info">
             <h6 class='first'>
              
-             <h4 class='name1'>$${product.price}
-             <i class='bx bx-plus' id='${product.id}'></i> 
+             <h4 class='name1'>
+             $${product.price}
+             ${product.quantity ? `<i class="bx bx-plus" id="${product.id}"></i>`
+                      : "<span class= 'soldOut'>SOLD OUT</span>"}
+             
+
+
              </h4>
             
              <span><b>Stock</b>: ${product.quantity}</span>
@@ -81,8 +86,9 @@ function handleShowCart(){
  
      window.localStorage.setItem("cart", JSON.stringify(bDatos.cart))
  
-      console.log(bDatos.cart)
+      
       printProductsInCart(bDatos)
+      printTotal(bDatos)
      }
     });
  
@@ -96,18 +102,18 @@ function printProductsInCart(bDatos){
 
    for (const product in bDatos.cart) {
     const {quantity, price, name, image, id, amount}= bDatos.cart[product]
-    console.log({quantity,price,name,image,id,amount})
+    
     html += 
     `
     <div class='card_product'>
        <div class='card_product--img'>
              <img src= '${image}' alt='image' />
        </div>
-       <div className='card_product--body'>
+       <div class='card_product--body'>
             <h4>${name} | $${price}</h4>
             <p>stock:${quantity}</p>
-        <div class='card_product--body-op'>
-        <i class='bx bx-minus' ></i>
+        <div class='card_product--body-op' id='${id}'>
+                    <i class='bx bx-minus' ></i>
                     <span>${amount}unit</span>
                     <i class='bx bx-plus' ></i>
                     <i class='bx bx-trash' ></i>
@@ -127,6 +133,107 @@ function printProductsInCart(bDatos){
 
 
 
+function handleProductsInCart(bDatos){
+    const cartProducts = document.querySelector('.card_products')
+
+   cartProducts.addEventListener('click', function(e){
+      if(e.target.classList.contains('bx-plus')){
+        const id = Number(e.target.parentElement.id)
+        const productFind = bDatos.products.find((product) => product.id === id)
+        if(productFind.quantity === bDatos.cart[productFind.id].amount) return alert('no tenemos mas en stock')
+
+
+
+        bDatos.cart[id].amount++
+      }
+
+      if(e.target.classList.contains('bx-minus')){
+        const id = Number(e.target.parentElement.id)
+        if(bDatos.cart[id].amount === 1){
+            const response = confirm('estas seguro de que lo quieres eliminar?')
+            if(!response) return
+            
+            delete bDatos.cart[id]
+        } else{
+            bDatos.cart[id].amount--
+        }
+        
+     }
+     
+     if(e.target.classList.contains('bx-trash')){
+        const id = Number(e.target.parentElement.id)
+        const response = confirm('estas seguro de que lo quieres eliminar?')
+            if(!response) return
+        delete bDatos.cart[id]
+     }
+     window.localStorage.setItem("cart", JSON.stringify(bDatos.cart))
+     printProductsInCart(bDatos)
+     printTotal(bDatos)
+
+   })
+}
+
+function printTotal(bDatos){
+    const infoTotal = document.querySelector('.info_total')
+    const infoAmount =document.querySelector('.info_amount')
+  
+       
+       let totalProducts = 0
+       let amountProducts = 0
+       for (const product in bDatos.cart) {
+             const{amount, price} = bDatos.cart[product]
+             totalProducts += price * amount
+             amountProducts += amount
+              
+          }
+          infoAmount.textContent = amountProducts + ' units'
+      infoTotal.textContent = '$' + totalProducts + '.00'
+}
+
+
+function handleTotal(bDatos){
+    const btnBuy = document.querySelector('.btn_buy')
+
+   btnBuy.addEventListener('click', function(){
+     if(!Object.values(bDatos.cart).length) return alert('no hay nada que comprar')
+
+    
+
+
+     const response = confirm('seguro que quieres comprar?')
+      if(!response) return
+
+     const currentProducts = []
+     for (const product of bDatos.products) {
+        const productCart = bDatos.cart[product.id]
+        if(product.id === productCart?.id){
+            currentProducts.push({
+                ...product,
+             quantity: product.quantity - productCart.amount,
+            })
+        }else{
+            currentProducts.push(product)
+        }
+        
+     }
+
+     bDatos.products = currentProducts
+     bDatos.cart = {}
+     window.localStorage.setItem('products', JSON.stringify(bDatos.products))
+     window.localStorage.setItem('cart', JSON.stringify(bDatos.cart))
+     printTotal(bDatos) 
+     printProductsInCart(bDatos)
+     printProducts(bDatos);
+   })
+
+}
+
+
+
+
+
+
+
 async function main(){
    const bDatos = {
         products: JSON.parse(window.localStorage.getItem('products')) ||  
@@ -137,8 +244,15 @@ async function main(){
    handleShowCart(); 
    addCartFromProducts(bDatos)
    printProductsInCart(bDatos)
+   handleProductsInCart(bDatos)
+   printTotal(bDatos)
+   handleTotal(bDatos)
+   
 
    
+    
+
+     
 }  
 main(); 
 
